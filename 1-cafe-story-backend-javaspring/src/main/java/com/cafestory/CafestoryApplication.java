96cf5c0140bdf3c62ac5ddc9cf2e4da792b95282
GThinh17 <trangiathinh0309@gmail.com>
2026-05-15 08:41:38 +0700

    add java spring boost project

---
+++ 1-cafe-story-backend-javaspring/src/main/java/com/cafestory/CafestoryApplication.java
@@ -0,0 +1,13 @@
+package com.cafestory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafestoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafestoryApplication.class, args);
+	}
+
+}
